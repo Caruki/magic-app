0084360f6405c09670dbcc89aff188f6624da54b
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-03-24 10:00:36 +0100

    Fix cardlinks (#8)
    * Try different fixes
    
    * Adjust styling to properly show cards with links
    
    * Fix showing order of allsets cards
    
    * Fix unnecessary props in styled component

--- src/components/CardExample.js
+++ src/components/CardExample.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import styled from '@emotion/styled';
+// import isPropValid from '@emotion/is-prop-valid';
 
 const CardImage = styled.img`
-  width: 20%;
+  width: 100%;
   flex-basis: auto;
   flex: 0 1;
   margin: 10px;
+
   @media (max-width: 450px) {
     width: 90%;
     flex: 0 1;
   }
 `;
 
-function CardExample({ card }) {
+const CardLink = styled.a`
+  width: 20%;
+  margin: 10px;
+
+  @media (max-width: 450px) {
+    width: 90%;
+  }
+`;
+
+//   {
+//   shouldForwardProp: prop => isPropValid(prop) && prop === 'as'
+// })(
+//   props => ({
+//   as: 'a'
+// }))`
+
+function CardExample({ card, href }) {
   return (
-    <CardImage
-      src={card.image_uris.border_crop}
-      setURL={card.set_search_uri}
-      alt=""
-    ></CardImage>
+    <CardLink href={href}>
+      <CardImage
+        src={card.image_uris.border_crop}
+        setURL={card.set_search_uri}
+        alt=""
+      />
+    </CardLink>
   );
 }
 
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -12,22 +12,22 @@ export default function Preview({ name }) {
           'https://api.scryfall.com/cards/search?order=name&q=type%3Agod+set%3Athb'
         ),
         fetch(
-          'https://api.scryfall.com/cards/2d70ea49-8911-4361-8515-a78f34f6e721'
+          'https://api.scryfall.com/cards/49d2a680-4f3b-4bfa-b77b-d2dfaced9f23'
         ),
         fetch(
           'https://api.scryfall.com/cards/e6784910-0204-4a39-bb38-50daa03e94c2'
         ),
         fetch(
-          'https://api.scryfall.com/cards/43f7dad3-fd67-4a6b-8daa-dbda75d995c5'
-        ),
-        fetch(
-          'https://api.scryfall.com/cards/49d2a680-4f3b-4bfa-b77b-d2dfaced9f23'
-        ),
-        fetch(
           'https://api.scryfall.com/cards/83e61813-c4c8-4e80-8808-ac5107966ee3'
         ),
         fetch(
+          'https://api.scryfall.com/cards/43f7dad3-fd67-4a6b-8daa-dbda75d995c5'
+        ),
+        fetch(
           'https://api.scryfall.com/cards/eee1d0b9-64c0-466e-978d-31e744f156d6'
+        ),
+        fetch(
+          'https://api.scryfall.com/cards/2d70ea49-8911-4361-8515-a78f34f6e721'
         )
       ]);
       const results = await Promise.all(
@@ -64,15 +64,21 @@ export default function Preview({ name }) {
       {isAllSet &&
         cards?.map(card => (
           <CardExample
+            src={card.image_uris.border_crop}
             key={card.id}
             card={card}
-            as="a"
             href={`/all/${card.set_name}`}
-          ></CardExample>
+          />
         ))}
-      ;
+
       {!isAllSet &&
-        cards?.map(card => <CardExample card={card} key={card.id} />)}
+        cards?.map(card => (
+          <CardExample
+            card={card}
+            key={card.id}
+            src={card.image_uris.border_crop}
+          />
+        ))}
     </CardsWrapper>
   );
 }
